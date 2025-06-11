ae95dadb0218502b40418d3f79f10cdeda2daf4d
arthurmov <officialamovsesyan@gmail.com>
2025-06-11 11:09:53 -0400

    added and made changes to console

--- src/main/java/com/pluralsight/ui/UserInterface.java
+++ src/main/java/com/pluralsight/ui/UserInterface.java
@@ -1,18 +1,33 @@
 package com.pluralsight.ui;
 
+import com.pluralsight.Console;
 import com.pluralsight.models.Employee;
 
 public class UserInterface {
 
+    public UserInterface() {
+        this.console = new Console();
+    }
+
+    private Employee currentEmployee;
+    private Console console;
+
     public void display() {
         System.out.println("Welcome to the Northwind Manager!");
-
+        currentEmployee = loginUser();
+        System.out.println("Welcome, " + this.currentEmployee.getFirstName() + "!");
+        showHomeMenu();
     }
 
 
 
     private Employee loginUser() {
-
+        String s = console.promptForString("Hit Enter to login as Matt", true);
+        Employee e = new Employee(1, "Matt", "Christenson");
+        return e;
     }
 
+    private void showHomeMenu() {
+        System.out.println("Home Menu here");
+    }
 }
